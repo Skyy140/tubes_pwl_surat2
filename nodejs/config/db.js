926ac0236871d85471bd85da1870ecac1e8eb9b4
alfnsn <alfiansantika@gmail.com>
2025-05-14 15:12:26 +0700

    coba ignore

--- nodejs/config/db.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("event", "root", "it12345", {
-  host: "localhost",
-  dialect: "mysql",
-});
-
-module.exports = sequelize;
